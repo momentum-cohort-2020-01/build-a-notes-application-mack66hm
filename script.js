a527faf798327973ad01a910e96483ff5c7c41ea
mack66hm <mack66hm@gmail.com>
2020-02-06 18:52:17 -0500

    tried to make stuff work need to debug

---
+++ script.js
@@ -0,0 +1,67 @@
+/* globals fetch, moment */
+function print (value) {
+  console.log(value)
+  return value
+}
+
+function q (selector) {
+  return document.querySelector(selector)
+}
+
+function getAllNotes () {
+  return fetch('http://localhost:3000/notes/', {
+    method: 'GET'
+  })
+    .then(response => response.json())
+}
+
+function createNotesHTML (notes) {
+  return `<ul id='notes-list'>${notes.map(note => `<li>${note.note}</li>`).join('')}</ul>`
+
+  // let notesStr = '<ul id='notes-list'>'
+  // for (const note of notes) {
+  //     notesStr += createNotesHTML(note)
+  // }
+  // notesStr += '</ul>'
+  // return notesStr
+}
+
+function createNoteHTML (note) {
+  return `<li data-note-id='${note.id}'>${note.note} <button class='delete'>Delete</li>`
+}
+
+function postNewNote (noteText) {
+  return fetch('http://localhost:3000/notes/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ note: noteText, done: false, created: SVGAnimateMotionElement().format() })
+  })
+    .then(response => response.json())
+}
+
+function renderNotesList (notes) {
+  const notesHTML = createNotesHTML(notes)
+  const notesSection = q('#notes')
+  notesSection.innerHTML = notesHTML
+}
+
+function renderNewNotes (note) {
+  const noteHTML = createNoteHTML(note)
+  const notesList = q('#notes-list')
+  notesList.insertAdjacentHTML('beforeend', noteHTML)
+}
+getAllNotes().then(renderNotesList)
+
+q('#new-note-form').addEventListener('submit', event => {
+  e.preventDefault()
+  const noteTextField = q('#note-text')
+  const noteText = noteTextField.value
+  noteTextField.value = ''
+  postNewNote(noteText).then(renderNewNote)
+})
+
+q('#notes').addEventListener('click', event => {
+  if (event.target.matches('.delete')) {
+    print('delete ' + event.target.parentElement.dataset.todoId)
+  }
+})
